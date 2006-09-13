c545d243ab19329481caece63493b6561a1e794e
tziade <devnull@localhost>
2006-09-13 14:23:05 +0000

    added toLatin9 helper (other helpers on the way)

---
+++ xmlrpc.py
@@ -0,0 +1,43 @@
+# (C) Copyright 2005 Nuxeo SARL <http://nuxeo.com>
+# Authors:
+# Tarek Ziade <tziade@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+""" xmlrpc utilities
+"""
+
+def toLatin9(obj):
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if isinstance(v, unicode):
+                v = _stringToLatin9(v)
+                obj[k] = v
+    elif isinstance(obj, unicode):
+        obj = _stringToLatin9(obj)
+    return obj
+
+
+def _stringToLatin9(s):
+    if s is None:
+        return None
+    else:
+        # Replace RIGHT SINGLE QUOTATION MARK (unicode only)
+        # by the APOSTROPHE (ascii and latin1).
+        # cf. http://www.cl.cam.ac.uk/~mgk25/ucs/quotes.html
+        s = s.replace(u'\u2019', u'\u0027')
+        #&#8217;
+        return s.encode('iso-8859-15', 'ignore')
