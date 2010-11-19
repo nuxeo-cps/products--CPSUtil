64824584d5d75775662e05569d8921e75e6930f0
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-11-19 19:56:17 +0100

    #837 refactor: avoid confusion with double ids of global methods

--- resourceregistry.py
+++ resourceregistry.py
@@ -33,8 +33,29 @@ class Resource(object):
     def html(self, **kw):
         raise NotImplementedError
 
+class GlobalMethodResource(Resource):
+    """For method (ZPT, .py) based resources.
+
+    The id is deduced from the method name."""
+
+    @classmethod
+    def register(cls, method_name):
+        rid = ('gmeth', cls.__name__, method_name)
+        if rid in _resources:
+            return rid
+        _resources[rid] = cls(method_name)
+        return rid
+
 class HtmlResource(Resource):
-    """A resource represented by the html fragment that includes it."""
+    """A resource represented by the html fragment that includes it.
+
+    >>> HtmlResource.register('foo',
+    ...    '<script type="text/javascript">foo();</script>')
+    >>> _resources['foo'].html()
+    '<script type="text/javascript">foo();</script>'
+    >>> _resources['foo'].html(base_url='/mycps/')
+    '<script type="text/javascript">foo();</script>'
+    """
     def __init__(self, html):
         self.fragment = html
 
@@ -42,7 +63,22 @@ class HtmlResource(Resource):
         """Return appropriate inclusion fragment."""
         return self.fragment
 
-class JavaScriptMethodResource(Resource):
+    @classmethod
+    def register(cls, rid, fragment):
+        if rid in _resources:
+            raise ValueError('Existing resource by this id : %r' % rid)
+        _resources[rid] = cls(fragment)
+
+
+class JSGlobalMethodResource(GlobalMethodResource):
+    """Represent JavaScript resources that are provided by a portal-wide method
+
+    >>> rid = JSGlobalMethodResource.register('prototype.js')
+    >>> _resources[rid].html()
+    '<script type="text/javascript" src="prototype.js"></script>'
+    >>> _resources[rid].html(base_url='/mycps/')
+    '<script type="text/javascript" src="/mycps/prototype.js"></script>'
+    """
 
     template = ('<script type="text/javascript" src="%(base_url)s%(name)s">'
                 '</script>')
@@ -64,6 +100,7 @@ class RequestResourceRegistry(object):
 
     def __init__(self, base_url):
         self.required = {}
+        self.dumped = set()
         self.base_url = base_url
 
     def require(self, resource_id, category=None):
@@ -74,39 +111,30 @@ class RequestResourceRegistry(object):
         cat_req.add(resource_id)
 
     def dumpCategory(self, category):
+        """Produce HTML code providing all the resources for that category.
+
+        Resources that have already been dumped are *not* included.
+        Therefore, the caller must ensure proper inclusion in the final
+        HTML document.
+        """
         cat_req = self.required.get(category, ())
+        dumped = self.dumped
         res = []
         for rid in cat_req:
+            if rid in dumped:
+                continue
             r = _resources.get(rid)
             if r is None:
                 logger.warn("Unknow resource id : %r", rid)
             res.append(r.html(base_url=self.base_url))
+            dumped.add(rid)
         return '\n'.join(res)
 
-def register_html_resource(rid, fragment):
-    """Register a resource represented by its html fragment inclusion.
+def require_resource(rid, category=None, context=None):
+    """Convenience function."""
+    get_request_resource_registry(context).require(rid, category=category)
 
-    >>> register_html_resource('foo',
-    ...    '<script type="text/javascript">foo();</script>')
-    >>> _resources['foo'].html()
-    '<script type="text/javascript">foo();</script>'
-    >>> _resources['foo'].html(base_url='/mycps/')
-    '<script type="text/javascript">foo();</script>'
-    """
-    _resources[rid] = HtmlResource(fragment)
-
-def register_js_method(rid, method_name):
-    """Register a javascript inclusion as another http request
-
-    >>> register_js_method('proto', 'prototype.js')
-    >>> _resources['proto'].html()
-    '<script type="text/javascript" src="prototype.js"></script>'
-    >>> _resources['proto'].html(base_url='/mycps/')
-    '<script type="text/javascript" src="/mycps/prototype.js"></script>'
-    """
-    _resources[rid] = JavaScriptMethodResource(method_name)
-
-def get_request_resource_registry(context=None):
+def get_request_resource_registry(context):
     if context is None:
         raise ValueError("need at least the context")
     request = context.REQUEST
