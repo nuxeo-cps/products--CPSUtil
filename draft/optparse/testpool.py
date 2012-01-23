916c57c4a39331c25f4477c83a7b085294aeb6c6
coligout <cgoutte@anybox.fr>
2012-01-23 03:00:06 +0100

    attempt of using StringIO is a fail, however args[$] hack still works ...

--- draft/optparse/testpool.py
+++ draft/optparse/testpool.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import pdb
 from random import random 
 import os
-
+from StringIO import StringIO
 
 def do_calculation(data):
     """
@@ -25,12 +25,20 @@ def do_calculation(data):
 
     return str(data * 2) + ':' + app
 
-def run_script(scripts = None):
+def run_script(args=None):
     """
     This function is intended to be a "buffer" for sub-processes that will be 
     created
     """
-    os.system('python '+command +' -b 243')
+    os.system('python '+args[0] +' -b 243')
+    sio=args[1]
+    if sio:
+        args[1].write(multiprocessing.current_process().name + ("-"*12) 
+                  +str(os.getpid()))
+        args[1].flush()
+        args[1].seek(0)
+        print(sio.read())
+        sio.seek(0)
 
 def start_process():
     print 'Starting', multiprocessing.current_process().name
@@ -39,24 +47,55 @@ def generate_scripts_names():
     """
     Pretty obvious for now but provides cleaner architecture
     """
-    scripts = ['rep1/fichier.py','rep2/fichier.py','rep3/fichier.py'] * 10
+    scripts = ['rep1/fichier.py','rep2/fichier.py','rep3/fichier.py'] * 2
     
-    #Pour ne pas avoir a ecrire 30 scripts
+    #Pour ne pas avoir a ecrire $n$ scripts
     
     return scripts
     
-    
+def generateStringIOs(list_or_int=1):
+    t = type(list_or_int)
+    if t == type([]) or t==type(()):
+        l=len(list_or_int)
+    elif type(list_or_int)==type(0):
+        l=list_or_int
+    #Il doit y avoir plus elegant/pythonique ...
+    else:
+        raise(TypeError)
+    #Comment mettre un message ?
 
+    res=[]
+    for i in range(l):
+        res.append(StringIO())
+    return res
+            
 if __name__ == '__main__':
 
     pool_size = multiprocessing.cpu_count() * 2
-    scripts = generate_scripts_name()
+    scripts = generate_scripts_names()
+    sios=generateStringIOs(scripts)
     print 'creating %r processes'%pool_size
-
+    
     pool = multiprocessing.Pool(processes=pool_size)
     
     #pool_outputs = pool.map(do_calculation, inputs)
-    pool_out2 = pool.map(run_script, scripts)
-    pool.close() # no more tasks
-    pool.join()  # wrap up current tasks
+    new_inputs=[]
+    #import pdb;pdb.set_trace()
+    for i in range(len(scripts)):
+        x=scripts[i]
+        y=sios[i]
+        y.write('test')
+        e=[x,y]
+        new_inputs.append(e)
+    
+    #not sure about arguments handling,thus i tried with this list
+    pool_out2 = pool.map(run_script,new_inputs)
+    pool.close()
+    pool.join()
  
+    
+  
+    import pdb;pdb.set_trace()
+    for s in sios:
+        s.seek(0)
+        print(s.read())
