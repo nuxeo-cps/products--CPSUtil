1c2db660d482d852f337d4a9fde5e5030a90fde6
coligout <cgoutte@anybox.fr>
2012-01-24 18:40:51 +0100

    Try to benchmark

--- draft/optparse/test3.py
+++ draft/optparse/test3.py
@@ -1,9 +1,18 @@
 import os 
+import subprocess
 import multiprocessing
+import shlex
 from time import clock
 from testpool import generate_scripts_names ,generateStringIOs
 
 #Merging with gracinet's idea
+
+"""
+This scripts aims to provide 
+Parrallel processing of CPS test.
+As they may take la long time to run we try to profile them as they take long to execute.
+
+"""
 
 class scriptObject():
     def __init__(self,script = '' , cmd = 'python' , options = ''):
@@ -12,22 +21,33 @@ class scriptObject():
         self.options = options
     
     def system_instr(self):
-        return ' '.join([self.cmd,self.script,self.options])
+        return shlex.split(' '.join([self.cmd,self.script,self.options]))
+        return [self.cmd , self.script,self.options]
 
     def __str__(self):
         return self.__dict__.__str__()
     
 
 def do_thing(scr_obj = None):
-    res={}
-    t1=clock()
+    
+    """
+    Wrap for pooling and launching processes
+    Here try to monitor time taken since real tasks may take quite a long time 
+    to complete. Then they the longest test should be lauch first so as to avoid
+    having to wait for them to complete
+    """
+    res = {}
+
+    t1 = clock()
     if not scr_obj:
         return "No task provided"
-    os.system( scr_obj.system_instr() )
-    print os.getpid()
+    ags = scr_obj.system_instr()
     
-    res['duration']=clock()-t1
-    
+    son = subprocess.Popen(ags)
+    print "spid %r pid %r "%(son.pid,os.getpid())
+    if os.waitpid(son.pid,0):
+        print times()
+        res['duration'] = clock()-t1
     return res
 
 if __name__ == '__main__':
@@ -37,8 +57,8 @@ if __name__ == '__main__':
     for x in scripts:
         print x.system_instr()
 
-    p = multiprocessing.Pool(processes = 2)
-    resultat = p.map(do_thing, scripts, 1)
+    p = multiprocessing.Pool(processes = 1)
+    resultat = p.map(do_thing, scripts)
 
     for x in resultat:
         print x['duration']
